3fefff71863ac618d17dfc849d833a813b2485a3
Keegan Kinnear <keegan@mailblaze.com>
2023-06-20 19:58:04 +0200

    2nd commit

--- app/index.php
+++ app/index.php
@@ -6,16 +6,29 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
     <body>
-        <div class="container overflow-hidden text-center">
-        <div class="row gx-5">
-            <div class="col">
-            <div class="p-3">Custom column padding</div>
+        <form>
+            <p class="h1"> Calculator Test </p>
+            <div class="container overflow-hidden text-center">
+            <div class="row gx-5">
+                <div class="col">
+                <div class="form-group">
+                    <label for="exampleFormControlInput1">Number One</label>
+                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                </div>
+                </div>
+                <div class="col">
+                <div class="form-group">
+                    <label for="exampleFormControlInput1">Number Two</label>
+                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                </div>
+                </div>
             </div>
-            <div class="col">
-            <div class="p-3">Custom column padding</div>
             </div>
-        </div>
-        </div>
+            <div class="form-group">
+                <label for="exampleFormControlTextarea1">Answer</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            </div>
+        </form>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
     </body>
 </html>
